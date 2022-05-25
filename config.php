79cc3d936185ddad953938f047974d222ba0253a
moeed31 <44394330+moeed31@users.noreply.github.com>
2022-05-25 21:38:39 +0600

    Add files via upload

---
+++ config.php
@@ -0,0 +1,25 @@
+<?PHP
+$public = ""; //Recaptcha Public Key
+$secret = ""; // Recaptcha Privat Key
+
+$ts3_host = "trv.ddnsgeek.com";
+$ts3_q_port = "10011";
+$ts3_s_port = "9987";
+$ts3_username = "serveradmin";
+$ts3_password = "password";
+$ts3_nick = "Channel Creator V2";
+
+$cpid = 20; //SPACER ID (the root) the created channels will be collected as sub channel there
+$chadmin_group_id = 9; // Channel Admin Group ID Default 5
+
+$channel_description = "";
+$channel_topic = "CHADD | WebUI @ ".date("H:i:s")." - ".date("d.m.Y");
+$server_conn_url = "ts3server://myip:port";
+$imprint_url = "";
+
+//BADWORDS will be replaced with XXXX
+$badwords = array("example", "example");
+
+// Allowed groups whitelist
+$allowed_groups = array();
+?>
